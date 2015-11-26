ff4fdc40970dff90587ec047a436869ec58cbafa
Muhsin Abdul-Musawwir <moose.techguy@gmail.com>
2015-11-25 19:20:34 -0800

    (feat) Add list items to Firebase

--- src/header.jsx
+++ src/header.jsx
@@ -2,14 +2,40 @@ var React = require('react');
 
 
 module.exports = React.createClass({
+  getInitialState: function() {
+      return {
+          text: ''
+      };
+  },
   render: function() {
     return  <div className="input-group">
-              <input type="text" className="form-control" />
+              <input 
+                placeholder="Add a new task"
+                value={this.state.text}
+                onChange={this.handleInputChange}
+                type="text" 
+                className="form-control" />
               <span className="input-group-btn">
-                <button className="btn btn-default" type="button">
+                <button 
+                  onClick={this.handleClick}
+                  className="btn btn-default" 
+                  type="button">
                   Add
                 </button>
-              </span> 
+              </span>
             </div>  
+  },
+  handleClick: function() {
+    // Send value of text input to Firebase
+    this.props.itemsStore.push({
+      text: this.state.text,
+      done: false
+    });
+
+    this.setState({text: ''});
+    console.log("I was clicked", this.state.text);
+  },
+  handleInputChange: function(event) {
+    this.setState({text: event.target.value})
   }
 });
